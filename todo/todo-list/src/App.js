b048282b34a1911bbd4e04454c8a4540cc3f82d0
kc0112 <Khushichavada0112@gmail.com>
2021-07-04 18:32:10 +0530

    update

---
+++ todo/todo-list/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header'
+import Loginc from './components/Loginc'
+import Signup from './components/Signup'
+import Contact from './components/Contact';
+import Task from './components/Task/Task'
+import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+
+
+function App() {
+
+  const [detail, setDetail] = useState([]);
+
+  const setKeys = (details) => {
+    console.log(details)
+    setDetail(details);
+  }
+
+  return (
+    <>
+      <Router>
+        <Header details={ detail} />
+        <Switch>
+          <Route path="/" exact render={(props) => (
+            <Loginc {...props}  setKeys={setKeys}/>
+          )} />
+
+        <Route path="/signup" render={(props) => (
+          <Signup {...props} setKeys={setKeys}/>
+          )} />
+          
+          <Route path="/home" render={(props) => (
+            <Task {...props} details={ detail}/>
+          )} />
+
+          <Route path="/contact" render={(props) => (
+            <Contact {...props} details={ detail}/>
+          )} />
+
+        </Switch>
+      </Router>
+      
+    </>
+  );
+}
+
+export default App;
